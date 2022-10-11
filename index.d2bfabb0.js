f27f3f130dfd98bdeb621e10b14d21664e5724fb
KharlanEd <109873871+KharlanEd@users.noreply.github.com>
2022-10-11 08:45:26 +0000

    Deploying to gh-pages from @ KharlanEd/command-project@4a097f3d227b97e54ddee6e0c2813f4a4080e50d 🚀

---
+++ index.d2bfabb0.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-locations]"),closeModalBtn:document.querySelector("[data-modal-close-locations]"),modal:document.querySelector("[data-modal-locations]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.d2bfabb0.js.map
---
+++ index.f93e6c70.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-franchise]"),closeModalBtn:document.querySelector("[data-modal-close-franchise]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.f93e6c70.js.map
