478b54248b9cf8787229518b22ae07c6abf3bc7c
Andrey Obibok <andreyobibok@gmail.com>
2022-10-14 14:21:52 +0300

    js-fix

---
+++ src/js/scroll.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+const refs = {
+  menu: document.querySelector('[data-menu]'),
+  body: document.querySelector('body'),
+};
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $('a').on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+      if (refs.menu.classList[1] === 'mob-is-hidden') {
+        return;
+      }
+      refs.menu.classList.toggle('mob-is-hidden');
+      refs.body.classList.toggle('no-scroll');
+      console.log(refs.menu.classList[1]);
+    }
+  });
+});
